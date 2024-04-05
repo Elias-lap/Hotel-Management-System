8d62ef462b069b55c95abfd94e5c3dadb7a59e95
Elias-lap <Eliaszerz92@gmail.com>
2024-04-06 01:29:31 +0200

    Start WIth Drawer

--- src/ShareModule/Components/Sidebar/Sidebar.tsx
+++ src/ShareModule/Components/Sidebar/Sidebar.tsx
@@ -1,6 +1,69 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Sidebar() {
+const Sidebar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open); // Toggle the state of 'open'
+  };
+
   return (
-    <div className='bg-info'>Sidebar</div>
-  )
-}
+    <Drawer
+      sx={{
+        width: 240,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: 230,
+          boxSizing: 'border-box',
+          background: "blue"
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+      open={open} // Specify the 'open' state of the drawer
+    >
+      <List>
+        <ListItem  key="Home" onClick={toggleDrawer}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem  key="About" onClick={toggleDrawer}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <ListItemIcon>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary="About" />
+        </ListItem>
+        {/* Add more ListItems for other sidebar items */}
+      </List>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
