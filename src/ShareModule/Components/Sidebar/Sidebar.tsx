2eb6f426007aeb7e6b18db4ed66e476350e45341
Elias-lap <Eliaszerz92@gmail.com>
2024-04-07 00:56:03 +0200

    SideBar

--- src/ShareModule/Components/Sidebar/Sidebar.tsx
+++ src/ShareModule/Components/Sidebar/Sidebar.tsx
@@ -1,69 +1,170 @@
-import React from 'react';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import { IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import UsersIcon from "@mui/icons-material/People";
+import { ListItemButton } from "@mui/material";
+import RoomsIcon from "@mui/icons-material/Hotel";
+import AdsIcon from "@mui/icons-material/LocalOffer";
+import BookingsIcon from "@mui/icons-material/Event";
+import FacilitiesIcon from "@mui/icons-material/LocationCity";
+import ChangePasswordIcon from "@mui/icons-material/Lock";
+import LogoutIcon from "@mui/icons-material/ExitToApp";
 
-const Sidebar = () => {
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen
+  }),
+  overflowX: "hidden"
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`
+  }
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: prop => prop !== "open"
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme)
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme)
+  })
+}));
+
+export default function MiniDrawer() {
   const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = () => {
-    setOpen(!open); // Toggle the state of 'open'
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
   return (
-    <Drawer
-      sx={{
-        width: 240,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: 230,
-          boxSizing: 'border-box',
-          background: "blue"
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-      open={open} // Specify the 'open' state of the drawer
-    >
-      <List>
-        <ListItem  key="Home" onClick={toggleDrawer}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-        <ListItem  key="About" onClick={toggleDrawer}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <ListItemIcon>
-            <InfoIcon />
-          </ListItemIcon>
-          <ListItemText primary="About" />
-        </ListItem>
-        {/* Add more ListItems for other sidebar items */}
-      </List>
-    </Drawer>
-  );
-};
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
 
-export default Sidebar;
+      <Drawer variant="permanent" open={open} sx={{ "& .MuiDrawer-paper": { backgroundColor: "blue" } }} >
+        <DrawerHeader>
+          {open ? (
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon sx={{color : "white" }} />
+            </IconButton>
+          ) : (
+            <IconButton onClick={handleDrawerOpen}>
+              <ChevronRightIcon  sx={{color : "white" }}/>
+            </IconButton>
+          )}
+        </DrawerHeader>
+
+        <List >
+          <ListItemButton  sx={{color : "white" }}  >
+            <ListItemIcon>
+              <IconButton>
+                <HomeIcon  sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Home" sx={{color : "white" }} />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+                <UsersIcon  sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Users" sx={{color : "white" }}/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+              <RoomsIcon  sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Rooms" sx={{color : "white" }}/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+              <AdsIcon sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Ads"  sx={{color : "white" }}/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+              <BookingsIcon sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Bookings" sx={{color : "white" }}/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+              <FacilitiesIcon  sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Facilities"  sx={{color : "white" }}/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+              <ChangePasswordIcon sx={{color : "white" }} />
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Change Password"  sx={{color : "white" }}/>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <IconButton>
+              <LogoutIcon sx={{color : "white" }}/>
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText primary="Logout" sx={{color : "white" }} />
+          </ListItemButton>
+          
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
